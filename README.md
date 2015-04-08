3093ce5e6ce4ed9120beecf652202c5362b6a608
Chris <chris.chattin@gmail.com>
2015-04-08 17:21:45 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# railscasts-downloader
-Quick script to download all of the amazing Railscasts to your computer.
+# Railscasts Downloader
+Ruby script to download all of Ryan Bate's Railscasts to your computer.
+
+Borrowed heavily (mostly copied) from clekstro.
---
+++ download.rb
@@ -0,0 +1,46 @@
+#!/usr/bin/env ruby
+# Quick script to download Railscasts episodes from RSS Feed
+# Based on https://github.com/donnykurnia/Railscasts-downloader
+
+require 'nokogiri'
+require 'open-uri'
+
+
+SUBSCRIPTION_KEY = '*****your subscription key here ***********'
+RSS_URI = 'http://railscasts.com/subscriptions/' + SUBSCRIPTION_KEY + '/episodes.rss'
+
+SAVE_PATH = '~/railscastsdownloader/episodes'
+VIDEO_FORMAT = '.mp4' # modify to whatever format you prefer (and is supported)
+
+CURRENT_DOWNLOADS = Dir["#{SAVE_PATH}/*#{VIDEO_FORMAT}"]
+
+def download_new_episodes
+  feed = Nokogiri::XML(open(RSS_URI))
+  feed.xpath("//enclosure").each do |episode|
+    episode_url = ensure_extension(episode[:url])
+    download_episode(episode_url) unless already_downloaded?(episode_url)
+  end
+end
+
+def already_downloaded?(episode)
+  episode = SAVE_PATH + '/' + episode.split('/').last
+  CURRENT_DOWNLOADS.include?(episode)
+end
+
+def download_episode(video_url)
+  filename = video_url.split('/').last
+  p "Downloading #{filename}"
+  local_file = SAVE_PATH + '/' + filename
+  %x(wget #{video_url} -c -O #{local_file}.tmp )
+  %x(mv #{local_file}.tmp #{local_file} )
+  p "Finished downloading #{filename}"
+end
+
+def ensure_extension(file)
+  ext = File.extname(file)
+  return file unless (ext != VIDEO_FORMAT)
+  file = file.gsub(ext, VIDEO_FORMAT)
+end
+
+download_new_episodes
+p "All episodes downloaded."
